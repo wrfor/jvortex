04e0144e1cecf117961b8ed6e57cbe4b21612295
wrfor <wrforall@gmail.com>
2026-08-03 15:10:35 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 function installApp() {
   alert("JVortex Dynamics App is installing!");
 }
-// Import Firebase SDK
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Firebase config (copy from Firebase Console)
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBjKbuuVy_NU9dTdiHhql0F8mHD5NIsI6k",
   authDomain: "admn-of-nexura-panel.firebaseapp.com",
   projectId: "admn-of-nexura-panel",
-  storageBucket: "admn-of-nexura-panel.appspot.com",
+  storageBucket: "admn-of-nexura-panel.firebasestorage.app",
   messagingSenderId: "34741499614",
   appId: "1:34741499614:web:5c5eb0fbc39384af16f44f",
   measurementId: "G-FHXSSC1PVL"
 };
 
-// Start Firebase
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);     // For text data (messages, services, customers)
-const storage = getStorage(app);  // For images/videos
+const analytics = getAnalytics(app);
